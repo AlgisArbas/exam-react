7b73d3c37123479bfbc1b084fd2b9240b5560958
AlgisArbas <arbaciauskas.a@gmail.com>
2021-11-22 14:13:20 +0200

    second example

--- src/exam-tasks/task-3/Task3.js
+++ src/exam-tasks/task-3/Task3.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Task3() {
+    // [age, setAge] = useState()
+    // const respond = (age) => {
+    //    return age >= 18 ? "Dėkojame, kad užsiregistravote" : "Deja, registracija negalima.  Registruotis galima tik nuo 18 metų"
+    // }
+    const ageSet = (e) => {
+        console.log(e)
+    }
+
   return (
     <div>
       <h3>Task 3</h3>
+        <form className='task3form'>
+            <input type='text' placeholder='name'/>
+            <input type='text' placeholder='surname'/>
+            <input type='number' placeholder='age'/>
+            <button onSubmit='{}' type='submit'>Registruoti</button>
+            <p>{}</p>
+        </form>
     </div>
   );
 }
